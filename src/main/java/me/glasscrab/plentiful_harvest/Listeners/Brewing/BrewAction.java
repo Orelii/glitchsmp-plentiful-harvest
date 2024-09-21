4e7f5af9ea2a21335a5045e5d6098ca09d5afae7
Orelii <83521686+Orelii@users.noreply.github.com>
2024-09-21 14:02:23 -0600

    fixed brewaction not having a credit comment

--- src/main/java/me/glasscrab/plentiful_harvest/Listeners/Brewing/BrewAction.java
+++ src/main/java/me/glasscrab/plentiful_harvest/Listeners/Brewing/BrewAction.java
@@ -3,6 +3,11 @@ package me.glasscrab.plentiful_harvest.Listeners.Brewing;
 import org.bukkit.inventory.BrewerInventory;
 import org.bukkit.inventory.ItemStack;
 
+/*
+ * This is not my code:
+ * https://www.spigotmc.org/threads/updated-creating-custom-brewing-recipes.472074/
+ */
+
 public abstract class BrewAction {
     public abstract void brew(BrewerInventory inventory, ItemStack item, ItemStack ingredient);
 }
